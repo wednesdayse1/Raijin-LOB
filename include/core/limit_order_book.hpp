976861c6348718206ded295927aafdad09b12e1d
LonelyGuy-SE1 <brawlers.assemble@gmail.com>
2026-05-09 10:34:17 +0530

    lob

--- include/core/limit_order_book.hpp
+++ include/core/limit_order_book.hpp
@@ -1,1 +1,31 @@
 #pragma once
+
+#include "types.hpp"
+#include "price_level.hpp"
+#include "order_pool.hpp"
+#include <cstdint>
+
+namespace raijin
+{
+    class LimitOrderBook
+    {
+    private:
+        OrderPool pool;
+        static constexpr size_t MAX_PRICE_TICKS = 100000;
+        PriceLevel *bid_levels[MAX_PRICE_TICKS];
+        PriceLevel *ask_levels[MAX_PRICE_TICKS];
+
+        uint64_t best_bid_price;
+        uint64_t best_ask_price;
+
+        PriceLevel *get_or_create_level(uint64_t price, bool is_buy);
+
+    public:
+        explicit LimitOrderBook(size_t pool_capacity);
+        ~LimitOrderBook();
+
+        void add_order(uint64_t order_id, uint64_t price, uint32_t volume, bool is_buy);
+        void cancel_order(Order *order);
+        void execute_order(Order *order, uint32_t execution_volume);
+    };
+}
